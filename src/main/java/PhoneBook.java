c4b0c9259459dc8815b4a1eee4ec50cd48ece985
KIRILL <kerillva@gmail.ru>
2025-06-16 19:43:00 +0400

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,13 @@
+import java.util.HashMap;
+
 public class PhoneBook {
 
+    private final HashMap<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) { // Проверяем наличие дубликатов имен
+            contacts.put(name, number);   // Если имя новое, добавляем контакт
+        }
+        return contacts.size();           // Возвращаем общее число записей
     }
 }
